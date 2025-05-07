27019f8f0fa3673c6bdfb9ad37bf1cd64c97f4de
Sangho Lee <sangholee45@outlook.com>
2025-05-07 09:40:55 +0800

    Remove winborder setting from mason
    Reason: mason supports winborder

--- init.lua
+++ init.lua
@@ -519,7 +519,7 @@ require('lazy').setup({
       -- Automatically install LSPs and related tools to stdpath for Neovim
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-      { 'williamboman/mason.nvim', opts = { ui = { border = g_border } } },
+      { 'williamboman/mason.nvim', opts = {} },
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
