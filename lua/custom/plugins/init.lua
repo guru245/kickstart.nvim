a32bcd1afcab9d645a1339f59cb6799e3f49cf0e
Sangho Lee <sangholee45@outlook.com>
2025-05-07 10:54:50 +0800

    Add more personal setting

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,47 @@
 --
 -- See the kickstart.nvim README for more information
 
+-- Turn on plugin and indent, depending on file type
+vim.cmd("filetype plugin indent on")
+
+vim.opt.wrap = false
+vim.opt.guicursor = ''
+
+-- Set tab size
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+-- Tell Vim to delete the white space at the start of the line, a line break
+-- and the character before where Insert mode started.
+vim.o.backspace = "indent,eol,start"
+
+-- Work for C-like programs, but can also be used for other languages
+vim.opt.smartindent = true
+
+-- Copy indent from current line when starting a new line. This should be
+-- on when smartindent is used.
+vim.opt.autoindent = true
+
+-- Set indent for switch statement in C. Just my cup of tea.
+vim.opt.cinoptions = ":0"
+
+-- Determine the 'fileencoding' of a file being opened.
+vim.o.fileencodings = "utf-8,cp949,cp932,euc-kr,shift-jis,big5,ucs-2le,latin"
+
+-- Represent data in memory
+vim.opt.encoding = "utf-8"
+
+-- Use only unix fileformat. "dos" can be added like "unix, dos"
+vim.opt.fileformats = "unix"
+
+-- Set 80, 100 column guideline
+vim.cmd 'set colorcolumn=100'
+vim.cmd 'let &colorcolumn="80,".join(range(100,100),",")'
+
+-----------------------
+-- Search Options
+-----------------------
 -- Not search wrap around the end of a file
 vim.opt.wrapscan = false
 
@@ -15,17 +56,12 @@ vim.opt.smartcase = true
 -- Jump to one to the other using %. Various character can be added.
 vim.opt.matchpairs:append '<:>'
 
-vim.opt.wrap = false
-vim.opt.guicursor = ''
-
--- Set 80, 100 column guideline
-vim.cmd 'set colorcolumn=100'
-vim.cmd 'let &colorcolumn="80,".join(range(100,100),",")'
-
+-----------------------
+-- Mappings
+-----------------------
 -- Move source code by tab size. Tab is right move and Shift+tab is left.
 vim.keymap.set('v', '<Tab>', '>gv')
 vim.keymap.set('v', '<S-Tab>', '<gv')
-
 vim.keymap.set('n', '<F2>', '<Cmd>w!<CR>')
 
 return {
