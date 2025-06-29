2121d662b5c5109da5b764548bd2b14e8a6f82eb
Sangho Lee <sangholee45@outlook.com>
2025-06-29 16:53:12 +0900

    Add dashboard

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -202,6 +202,14 @@ return {
           },
         },
       },
+      dashboard = {
+        sections = {
+          { section = 'header' },
+          { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+          { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+          { section = 'startup' },
+        },
+      },
     },
     keys = {
       -- stylua: ignore start
